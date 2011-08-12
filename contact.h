9dea984c899b8693aae1af4cc87173b17aba0390
Tyson Key <tyson.key@gmail.com>
2011-08-12 20:12:18 +0100

    Attempt to define a set of Contact data type fields

--- contact.h
+++ contact.h
@@ -1,10 +1,23 @@
 #ifndef CONTACT_H
 #define CONTACT_H
 
+#include <QString>
+
 class Contact
 {
 public:
     Contact();
+
+protected:
+    int iVersion;
+    qint64 iUid;
+    QString iForename;
+    QString iSurname;
+    QString iEMailAddress;
+    QString iTwitterUrl;
+    QString iSkypeUserName;
+    int iSkypeStatus;
+    QString iLastFmUserName;
 };
 
 #endif // CONTACT_H
