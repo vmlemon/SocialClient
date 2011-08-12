f63c59a8c85967d1df376d65e1ee3f83a191a0d6
Tyson Key <tyson.key@symbiandevco.org>
2011-08-12 18:20:41 +0100

    Refactor the Skype feed parser out into its own class

---
+++ Parsers/skype.h
@@ -0,0 +1,26 @@
+#ifndef SKYPE_H
+#define SKYPE_H
+
+#include <QString>
+
+enum TSkypeStatus {
+    EUnknown = 0,
+    EOffline = 1,
+    EOnline = 2,
+    EAway = 3,
+    ENotAvailable = 4,
+    EDoNotDisturb = 5,
+    EInvisible = 6,
+    ESkypeMe = 7
+};
+
+class Skype
+{
+public:
+    Skype();
+    QString ParseSkypeStatus(QString aStatusData);
+
+private:
+};
+
+#endif // SKYPE_H
