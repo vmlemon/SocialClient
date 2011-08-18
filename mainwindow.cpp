8bfc86de52e8dde398ae2066b73152480d13fc42
Tyson Key <tyson.key@gmail.com>
2011-08-18 23:02:16 +0100

    Syntax update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -222,5 +222,5 @@ void MainWindow::on_actionUpdate_Ticker_triggered()
 void MainWindow::on_actionCreate_Contact_triggered()
 {
     ContactBuilder *cb = new ContactBuilder();
-    cb.show();
+    cb->show();
 }
