dad05d14d90909f5b885a5af36bc104649368fcd
Tyson Key <tyson.key@symbiandevco.org>
2011-08-25 15:56:25 +0100

    Format example

--- Parsers/pidgin.cpp
+++ Parsers/pidgin.cpp
@@ -4,6 +4,12 @@ Pidgin::Pidgin()
 {
 }
 
+/*
+"MIME-Version: 1.0
+Content-Type: application/x-im-contact
+X-IM-Protocol: msn
+X-IM-Username: user@example.com
+  */
 QString Pidgin::GetXImContactProtocol(QByteArray aData) {
     QString contactProtocol;
 
