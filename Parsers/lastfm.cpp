0503431cba557df003c94c5f623b427d16464032
Tyson Key <tyson.key@gmail.com>
2012-03-19 13:57:39 +0000

    Convert the old GLFMLT() into a wrapper that uses the new API with a spaced hyphen as its separator

--- Parsers/lastfm.cpp
+++ Parsers/lastfm.cpp
@@ -7,42 +7,7 @@ LastFm::LastFm()
 
 QString LastFm::GetLastFmLatestTrack(QString aXmlData) {
 
-    QString workingPayload = aXmlData.remove("</track>").remove("</recenttracks>").simplified();
-    QString artistElement;
-    QString nameElement;
-
-    QString artistHyphenTitle;
-
-    int artistOffset = aXmlData.indexOf("<artist");
-    int nameOffset = 0;
-
-    if (aXmlData.length() !=0) {
-
-        /* Strip away everything before <artist> */
-        artistElement = workingPayload.remove(0, artistOffset + 40);
-        artistOffset = workingPayload.indexOf("<artist");
-
-        /* Toss artistElement's content back upstream... */
-        workingPayload = artistElement;
-
-        /* Extract the artist metadata */
-        artistElement.truncate(artistElement.indexOf("</artist>"));
-
-        /* Quick-and-dirty name fetching */
-        nameOffset = workingPayload.indexOf("<name>");
-        nameElement = workingPayload;
-        nameElement.remove(0, artistOffset + artistElement.size() + 1);
-        nameElement.remove("</artist> <name>");
-        nameElement.truncate(nameElement.indexOf("</name>"));
-
-        artistHyphenTitle.append(artistElement).append(" - ").append(nameElement);
-
-        return artistHyphenTitle;
-    }
-
-    else {
-        return "No data";
-    }
+    return GetLastFmLatestTrack(aXmlData, " - ");
 }
 
 QString LastFm::GetLastFmLatestTrack(QString aXmlData, QString aFormat) {
