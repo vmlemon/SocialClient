7084545761f88ed084c1a43737712390ac41108e
Tyson Key <tyson.key@gmail.com>
2012-03-19 18:16:28 +0000

    Attempt to set the text codec using the example code at http://stackoverflow.com/questions/7667407/i-lose-unicodeness-when-qdebuging-after-instancing-a-qapplication

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+        QTextCodec::setCodecForCStrings(codec);
+
     w.show();
 
     return a.exec();
