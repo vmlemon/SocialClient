7b233b06758261021fbe23137933e09a6fd4dc1a
Tyson Key <tyson.key@gmail.com>
2012-11-03 15:47:18 +0000

    Replace the cache-access method variables

--- lastfmcache.h
+++ lastfmcache.h
@@ -1,10 +1,15 @@
 #ifndef LASTFMCACHE_H
 #define LASTFMCACHE_H
 
+#include <QString>
+
 class LastFmCache
 {
 public:
     LastFmCache();
+    static QString GetDefaultCacheDir();
+    static void WriteToCache(QString aUsername, QString aData);
+    static QString ReadFromCache(QString aUsername);
 };
 
 #endif // LASTFMCACHE_H
