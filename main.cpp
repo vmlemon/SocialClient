73f83853fad65058ebbbdd69a5b69a4d390dd824
Tyson Key <tyson.key@gmail.com>
2015-09-13 14:10:31 +0100

    Version check hack for setting the text codec

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include "mainwindow.h"
 #include <QTextCodec>
+#include <QtGlobal>
 
 int main(int argc, char *argv[])
 {
@@ -8,8 +9,16 @@ int main(int argc, char *argv[])
     MainWindow w;
 
     QTextCodec *codec = QTextCodec::codecForName("UTF-8");
-        QTextCodec::setCodecForCStrings(codec);
 
+    //This is something of a hack...
+//#if QT_VERSION == 5.3.2
+#if QT_VERSION >= QT_VERSION_CHECK(5, 3, 2)
+        QTextCodec::setCodecForLocale(codec);
+#else
+        QTextCodec::setCodecForCStrings(codec);
+#endif
+
+        //QTextCodec::
     w.show();
 
     return a.exec();
